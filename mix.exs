b90842f20f2541abd1a384a3fe6ac9396db8df14
Olexandr Diacheko <olexandr.diachenko@gmail.com>
2018-01-10 17:39:40 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EmptyParams.Mixfile do
   def project do
     [
       app: :empty_params,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
